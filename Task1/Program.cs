70b017123f31c7a5c09be1b93c03a8a31516adb2
Anthony <gugl382@gmail.com>
2022-11-24 20:31:14 +0300

    Task1 modified

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,13 +2,13 @@
 // и показывает наибольшую цифру этого числа
 
 Random random = new Random();
-int number = random.Next(10,100);
+int number = random.Next(10, 100);
 Console.WriteLine(number);
 
-int firstNumber = number/10;
+int firstNumber = number / 10;
 int lastNumber = number % 10;
 
-if(firstNumber > lastNumber)
+if (firstNumber > lastNumber)
 {
     Console.WriteLine(firstNumber);
 }
