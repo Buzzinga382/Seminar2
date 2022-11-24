b2562eba3039aee96896488ee877b56fd02043af
Anthony <gugl382@gmail.com>
2022-11-24 20:18:08 +0300

    Task1 completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру этого числа
+
+Random random = new Random();
+int number = random.Next(10,100);
+Console.WriteLine(number);
+
+int firstNumber = number/10;
+int lastNumber = number % 10;
+
+if(firstNumber > lastNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+
+else
+{
+    Console.WriteLine(lastNumber);
+}
